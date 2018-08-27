394fbdb5180fe9366685823bc3473f8d43492a58
leelab-co-kr <42727027+leelab-co-kr@users.noreply.github.com>
2018-08-27 02:53:11 +0000

    Add files via upload

---
+++ foodchain_common_lib.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from hashlib import md5
+from pymongo import MongoClient
+
+def make_md5(s, encoding='utf-8'): 
+	return md5(s.encode(encoding)).hexdigest() 
+
+
+def get_member(mb_table, mb_id ) : 
+	rows = mb_table.find({ 'mb_id': mb_id })
+	for row in rows :
+		break
+	return row
