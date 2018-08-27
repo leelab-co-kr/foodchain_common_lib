23d12b431424ed1aee92f292cbf843f526c98636
leelab-co-kr <42727027+leelab-co-kr@users.noreply.github.com>
2018-08-27 03:48:55 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='foodchain_common_lib',
+    version='0.0.1',
+    description='foodchain common library.',
+    author='leelab',
+    url='https://github.com/leelab/foodchain_common_lib',
+    py_modules=['foodchain_common_lib'],
+    license="MIT Licence",
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
